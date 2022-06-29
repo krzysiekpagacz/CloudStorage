db5321db7e4261dc4a45def94cd4e08d1196552f
krzysiek pagacz <krzysiekpagacz@gmail.com>
2022-06-29 18:05:07 +0200

    NoteMapper functions added

--- CloudStorage/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
+++ CloudStorage/starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/mapper/NoteMapper.java
@@ -2,10 +2,12 @@ package com.udacity.jwdnd.course1.cloudstorage.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.udacity.jwdnd.course1.cloudstorage.model.Note;
 
@@ -15,11 +17,20 @@ public interface NoteMapper {
 	@Select("SELECT * FROM NOTES WHERE notetitle=#{noteTitle}")
 	Note getNoteByTitle(String noteTitle);
 	
+	@Select("SELECT * FROM NOTES")
+	List<Note> getAllNotes();
+	
+	@Select("SELECT * FROM NOTES WHERE userid = #{userId}")
+	List<Note> getUserNotes(Integer userId);
+	
 	@Insert("INSERT INTO NOTES (notetitle, notedescription, userid) VALUES(#{noteTitle}, #{noteDescription}, #{userId})")
 	@Options(useGeneratedKeys = true, keyProperty = "noteId")
 	int insertNote(Note note);
 	
-	@Select("SELECT * FROM NOTES")
-	List<Note> getAllNotes();
+	@Delete("DELETE FROM NOTES WHERE noteid = #{noteI}")
+	void deleteNote(Integer noteId);
+	
+	@Update("UPDATE NOTES SET notetitle = #{noteTitle}, notedescription = #{noteDescription} WHERE noteid = #{noteId}")
+	int updateNote(Note note);
 	
 }
